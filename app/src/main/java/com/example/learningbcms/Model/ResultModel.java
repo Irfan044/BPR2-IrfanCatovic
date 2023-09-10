c5917d89684edbf676b91e77b0419ca0fdc6a5d0
Irfan044 <50443187+Irfan044@users.noreply.github.com>
2023-09-10 11:51:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/learningbcms/Model/ResultModel.java
@@ -0,0 +1,22 @@
+package com.example.learningbcms.Model;
+
+import com.example.learningbcms.views.ResultFragment;
+
+public class ResultModel {
+
+    private int correct ,wrong, notAnswered;
+
+    public ResultModel(){}
+
+    public int getWrong() {
+        return wrong;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public int getNotAnswered() {
+        return notAnswered;
+    }
+}
