9da337218acc2c3505f564d28e8ecb842f08af37
Irfan044 <50443187+Irfan044@users.noreply.github.com>
2023-10-24 15:36:43 +0200

    Lesson Menu Fragment added

--- app/src/main/java/com/example/learningbcms/views/LessonMenuFragment.java
+++ app/src/main/java/com/example/learningbcms/views/LessonMenuFragment.java
@@ -1,66 +1,74 @@
 package com.example.learningbcms.views;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 import com.example.learningbcms.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LessonMenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LessonMenuFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LessonMenuFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LessonMenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LessonMenuFragment newInstance(String param1, String param2) {
-        LessonMenuFragment fragment = new LessonMenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lesson_menu, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_lesson_menu, container, false);
+
+        CardView cardView1 = view.findViewById(R.id.cardView1);
+        CardView cardView2 = view.findViewById(R.id.cardView2);
+        CardView cardView3 = view.findViewById(R.id.cardView3);
+        CardView cardView4 = view.findViewById(R.id.cardView4);
+        CardView cardView5 = view.findViewById(R.id.cardView5);
+        CardView cardView6 = view.findViewById(R.id.cardView6);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the first CardView
+                // You can launch an activity or perform any action here
+            }
+        });
+
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the second CardView
+            }
+        });
+
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the third CardView
+            }
+        });
+
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the fourth CardView
+            }
+        });
+
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the fifth CardView
+            }
+        });
+
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the sixth CardView
+            }
+        });
+
+        return view;
     }
 }
